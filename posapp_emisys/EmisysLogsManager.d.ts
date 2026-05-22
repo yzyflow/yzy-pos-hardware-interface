d0263258d93e74c49878a5c43f9b448162e428fd
Frederic Marchal <fmarchal@yzyflow.com>
2026-05-22 11:18:21 +0200

    Document functions added to the bridge-react to manage the log.

--- posapp_emisys/EmisysLogsManager.d.ts
+++ posapp_emisys/EmisysLogsManager.d.ts
@@ -5,6 +5,9 @@ declare interface LogsModel {
   extra?: unknown;
 }
 
+type getLogListCallback = (logs: LogsModel[]) => void;
+type getChangeCounterCallback = (counter: number) => void;
+
 export default class EmisysLogsManager {
   /**
    * Add info message to logs
@@ -32,4 +35,27 @@ export default class EmisysLogsManager {
    * @return LogsModel[]
    */
   getLogs(): LogsModel[];
+
+  /**
+   * Purge the log of the bridge.
+   */
+  clearLogs(): void;
+
+  /**
+   * Set the maximum number of log lines to keep in memory.
+   * @param {number} length How many entries to keep in the log. Older entries are discarded.
+   */
+  setMaxLogListLength(length: number): void;
+
+  /**
+   * Get a counter incremented each time an entry is added to the log.
+   * @param {getChangeCounterCallback} callback Function called with the counter value.
+   */
+  getChangeCounter(callback: getChangeCounterCallback)
+
+  /**
+   * Get the log entries as a list.
+   * @param {getLogListCallback} callback Function called when the list is ready.
+   */
+  getLogList(callback: getLogListCallback): void;
 }
