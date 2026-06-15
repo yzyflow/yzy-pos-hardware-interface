998d20c4fbe9a1d0f4afbd8d6de8611bb0291792
Frederic Marchal <fmarchal@yzyflow.com>
2026-06-15 17:10:28 +0200

    Add barcode scanner callback
    Instead of polling the bridge for a new barcode, it is possible to register
    a callback that get's called when a barcode is scanned.
    
    Version 1.1.6.

--- posapp_emisys/EmisysLaser.d.ts
+++ posapp_emisys/EmisysLaser.d.ts
@@ -1,3 +1,5 @@
+export type ScannedBarcodeCallback = (barcode: string) => void;
+
 export default class EmisysLaser {
   /**
    * Init laser listener
@@ -8,4 +10,16 @@ export default class EmisysLaser {
    * Get scanned barcode
    */
   getScannedBarcode(): string;
+
+  /**
+   * Register a callback to be called when a barcode or a QR code is scanned by the device.
+   * The scan is usually triggered by the user pressing on some button on the device.
+   * @return {number} The index to pass to removeScannedBarcodeListener() to remove this callback.
+   */
+  setScannedBarcodeListener(callback: ScannedBarcodeCallback | null): number;
+
+  /**
+   * Remove the scanned callback installed by setScannedBarcodeListener().
+   */
+  removeScannedBarcodeListener(index: number): void;
 }
