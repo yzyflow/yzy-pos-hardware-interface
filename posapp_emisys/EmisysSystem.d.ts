d92d3e5b389b7d70f32e421f647eb61ea63bc1b9
John-Nathan <jnmichielsens@emisys.com>
2026-07-09 17:26:05 +0200

    update OrientationType

--- posapp_emisys/EmisysSystem.d.ts
+++ posapp_emisys/EmisysSystem.d.ts
@@ -2,7 +2,14 @@
 import EmisysSystemComputer from './EmisysSystemComputer';
 
 type BatteryLevelType = 'low' | 'high' | 'critical';
-export type OrientationType = 'PORTRAIT_UP' | 'PORTRAIT_DOWN'| 'LANDSCAPE_LEFT'| 'LANDSCAPE_RIGHT'
+export type OrientationType =
+    | 'LANDSCAPE_RIGHT'
+    | 'LANDSCAPE_LEFT'
+    | 'LANDSCAPE'
+    | 'PORTRAIT_UP'
+    | 'PORTRAIT_DOWN'
+    | 'PORTRAIT'
+    | 'ALL';
 
 type IsInstalledAppCallbackType = (installed: boolean) => void;
 
